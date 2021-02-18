48482a755ee7cd8986ee07ae7ef86d627644b50f
Carlos Solano <carlos.solanogomez@hotmail.com>
2021-02-18 17:31:31 +0100

    Modificacion de SnakeCase a Camel Case

---
+++ scriptpunto1.js
@@ -0,0 +1,7 @@
+let frasesita = "Me_Gusta_Viajar_Por_El_Mundo";
+
+function camelcaseminuscula(frasesita) {
+    return frasesita.charAt(0).toLowerCase() + frasesita.slice(1).replaceAll("_", "");
+}
+
+console.log(camelcaseminuscula(frasesita));
